2b83781cfdc7ae5072738d00d4ff8264315a542b
Marcus Ng <marcusng@Marcuss-MBP.fios-router.home>
2017-03-31 17:12:13 -0400

    Updated Interface

--- DLLDeque.java
+++ DLLDeque.java
@@ -1,5 +1,13 @@
+// StrawberryOctoElephants
+// Marcus Ng, Ish Mahdi, and Thomas Lin
+// LAB #02: All Hands on Deque! (Not Schenectady; rather, synecdoche.)
+// 2017-3-31
+
 public class DLLDeque<T> implements Deque<T> {
 
     
+    public static void main(String[] args) {
+
+    }
     
 }
--- Deque.java
+++ Deque.java
@@ -1,17 +1,29 @@
+// StrawberryOctoElephants
+// Marcus Ng, Ish Mahdi, and Thomas Lin
+// LAB #02: All Hands on Deque! (Not Schenectady; rather, synecdoche.)
+// 2017-3-31
+
 public interface Deque<E> {
-   
-    public E addFirst( T x );
 
-    public E addLast( T x );
+    // Add element to the front w/o violating capacity restrictions
+    public void addFirst( T x );
 
-    public E removeFirst();
+    // Add element to the end w/o violating capacity restrictions
+    public void addLast( T x );
 
-    public E removeLast();
+    // Remove and return first element, or return null if empty
+    public E pollFirst();
 
+    // Remove and return last element, or return null if empty
+    public E pollLast();
+
+    // Return first element
     public E peekFirst();
 
+    // Return last element
     public E peekLast();
 
+    // Return the number of elements
     public int size();
     
 }
